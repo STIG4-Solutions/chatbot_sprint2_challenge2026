6c35b66497e2bcd478cacf8fbfc5eb90894b56fd
glaucokelly <glaucokelly@icloud.com>
2026-06-15 16:47:56 -0300

    fix: corrige import RecursiveCharacterTextSplitter para langchain-text-splitters

--- chatbot.py
+++ chatbot.py
@@ -20,7 +20,7 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
